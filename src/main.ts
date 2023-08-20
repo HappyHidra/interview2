20d52985c8be67ffaafc22f5d416b69ad8f759dd
DeepReap <okmmiha@mail.ru>
2023-08-21 01:07:32 +0300

    gh pages fixes adaptive layout

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 import MainPage from '@/pages/MainPage.vue'
 import AdaptivePage from '@/pages/AdaptivePage.vue'
 
-const routeInfos = [
-  {
-    path: '/interview2/',
-    component: MainPage
-  },
-  {
-    path: '/interview2/layout',
-    component: AdaptivePage
-  }
-]
-
 const router = createRouter({
   history: createWebHistory(),
-  routes: routeInfos
+  routes: [
+    { name: 'main', component: MainPage, path: '/interview2/' },
+    { name: 'layout', component: AdaptivePage, path: '/interview2/layout/' }
+  ]
 })
 
 createApp(App).use(router).mount('#app')
